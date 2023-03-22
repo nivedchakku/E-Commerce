b568ed9896828a06d1f81e3e1ad070db0ef6fa4b
Nived R <86557315+nivedchakku@users.noreply.github.com>
2023-03-22 12:25:27 +0530

    Add files via upload

---
+++ include/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+
+  <style>
+
+</style>
+</head>
+
+<body>
+
+
+
+<?php include("header.php");?>
+</div>
+
+
+<div class="content">
+<?php
+for($i=1;$i<100;$i++)
+{
+    echo "hello<br>";
+}
+?>
+</div>
+
+</body>
+</html>
